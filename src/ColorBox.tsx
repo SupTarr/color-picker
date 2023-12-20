df2513a1f92d77fc533732685d484635feac418c
Supakrit Chuchatwannakul <tatasuppakrit@gmail.com>
2023-12-20 11:12:13 +0700

    feat: format with prettier

--- src/ColorBox.tsx
+++ src/ColorBox.tsx
@@ -10,8 +10,12 @@ const ColorBox = ({ color }: PropsType) => {
       className="color-box w-full max-w-xs h-20 flex flex-col flex-wrap content-center justify-center border-2 border-slate-50 rounded-lg mb-5"
       style={{ backgroundColor: color }}
     >
-      <p className="mix-blend-normal text-center">{color ? color : "Empty value"}</p>
-      <p className="mix-blend-normal text-center">{color ? colorNames(color) : null}</p>
+      <p className="mix-blend-normal text-center">
+        {color ? color : "Empty value"}
+      </p>
+      <p className="mix-blend-normal text-center">
+        {color ? colorNames(color) : null}
+      </p>
     </section>
   );
 };
